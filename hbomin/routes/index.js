2b6341ab679e6b9419b9f4b35a76f384475390d6
KevinSpa2 <kevin.spalink@student.nhlstenden.com>
2023-12-04 15:17:09 +0100

    Connection with database

--- hbomin/routes/index.js
+++ hbomin/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const app = express();
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/hello', (req, res) => {
+  res.render('hello', { title: 'Express' });
 });
 
 module.exports = router;

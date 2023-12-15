c457c3b0fe98286a3b8919b53960882ed5e1eae9
AmanTrechsel <112391335+AmanTrechsel@users.noreply.github.com>
2023-12-15 10:22:35 +0100

    query abstraction

---
+++ hbomin/query.js
@@ -0,0 +1,42 @@
+var connection = require('./database');
+var xml = require('xml');
+
+function runQuery(dbQuery, callback) {
+  connection.query(dbQuery, (error, results) => {
+    if (error) {
+      callback(null, error); // Pass error to the callback
+    } else {
+      callback(results, null); // Pass results to the callback
+      console.log(results);
+    }
+  });
+}
+
+function outputJSON(dbQuery, router) {
+  runQuery(dbQuery, function(results, error) {
+    router.get('/', function(req, res) {
+      if (error) {
+        console.error('Fout bij het uitvoeren van de query: ', error);
+        res.status(500).json({ error: 'An error occurred' }); // Sending error as JSON response
+      } else {
+        res.json({ results:results[0] }); // Sending results as JSON response
+      }
+    });
+  });
+};
+
+function outputXML(dbQuery, router) {
+  runQuery(dbQuery, function(results, error) {
+    router.get('/', function(req, res) {
+      res.set('Content-Type', 'text/xml');
+      if (error) {
+        console.error('Fout bij het uitvoeren van de query: ', error);
+        res.status(500).send(xml({ error: 'An error occurred' })); // Sending error as XML response
+      } else {
+        res.send(xml({ results:results[0] })); // Sending results as XML response
+      }
+    });
+  });
+};
+
+module.exports = {outputXML, outputJSON};
--- hbomin/routes/users.js
+++ hbomin/routes/users.js
@@ -1,28 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var query = require('../query');
 
-const connection = require('../database');
-
-router.get('/', function(req, res, next) {
-  test(function(results, error) {
-    if (error) {
-      console.error('Fout bij het uitvoeren van de query: ', error);
-      res.status(500).json({ error: 'An error occurred' }); // Sending error as JSON response
-    } else {
-      res.json({ results:results[0] }); // Sending results as JSON response
-    }
-  });
-});
-
-function test(callback) {
-  connection.query('CALL get_usernames', (error, results) => {
-    if (error) {
-      callback(null, error); // Pass error to the callback
-    } else {
-      callback(results, null); // Pass results to the callback
-      console.log(results);
-    }
-  });
-}
+query.outputJSON('CALL get_usernames;', router);
 
 module.exports = router;

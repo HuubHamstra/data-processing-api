f82ebb0fbf729b6fc84cf3f4e2b996ffff2143a2
AmanTrechsel <112391335+AmanTrechsel@users.noreply.github.com>
2024-01-15 13:43:20 +0100

    register & login encrypting

--- hbomin/routes/login.js
+++ hbomin/routes/login.js
@@ -1,38 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const query = require('../query');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 const { secretKey, refreshTokens } = require('./config'); // Import shared configurations
 
 // Handle POST request for login
 router.post('/', (req, res) => {
   const { email, password } = req.body;
 
-  // Dummy authentication
-  if (email === 'user@example.com' && password === 'password') {
-    // Generate JWT token upon successful login
-    const accessToken = jwt.sign({ email }, secretKey, { expiresIn: '15m' });
+  let login_data = query.outputJSON(`CALL get_login_data(${email})`, router);
 
-    // Generate and store refresh token
-    const refreshToken = generateRefreshToken();
-    refreshTokens[email] = refreshToken;
+  verifyPassword(password, login_data).then(is_match => {
+    if (is_match) {
+      // Generate JWT token upon successful login
+      const accessToken = jwt.sign({ email }, secretKey, { expiresIn: '15m' });
 
-    // Set access token as a regular cookie
-    res.cookie('accessToken', accessToken, { maxAge: 900000, httpOnly: true });
+      // Generate and store refresh token
+      const refreshToken = generateRefreshToken();
+      refreshTokens[email] = refreshToken;
 
-    // Set refresh token as an HttpOnly and secure cookie
-    res.cookie('refreshToken', refreshToken, { maxAge: 3600000, httpOnly: true, secure: true, sameSite: 'Strict' });
+      // Set access token as a regular cookie
+      res.cookie('accessToken', accessToken, { maxAge: 900000, httpOnly: true });
 
-    res.json({ accessToken, refreshToken });
-  } else {
+      // Set refresh token as an HttpOnly and secure cookie
+      res.cookie('refreshToken', refreshToken, { maxAge: 3600000, httpOnly: true, secure: true, sameSite: 'Strict' });
+
+      res.json({ accessToken, refreshToken });
+    } else {
+      res.status(401).json({ message: 'Invalid username or password' });
+    }
+  })
+  .catch(() => {
     res.status(401).json({ message: 'Invalid username or password' });
-  }
+  });
 });
 
 // Function to generate the refresh token
 function generateRefreshToken() {
   const refreshKey = crypto.randomBytes(32).toString('hex');
   return jwt.sign({}, refreshKey, { expiresIn: '3h' });
-}
+};
+
+async function verifyPassword(password, hashedPassword) {
+  return await bcrypt.compare(password, hashedPassword);
+};
 
 module.exports = router;
--- hbomin/routes/register.js
+++ hbomin/routes/register.js
@@ -1,17 +1,38 @@
-var express = require('express');
-var router = express.Router();
-var query = require('../query');
+const express = require('express');
+const router = express.Router();
+const query = require('../query');
+const bcrypt = require('bcrypt');
 
-const connection = require('../database');
+router.post('/', (req, res) => {
+  const { full_name, email, password } = req.body;
+  const { first_name, last_name } = splitFullName(full_name);
 
-function executeCreateAccount(fname, lname, email, password, pay_method, sub_id, prof_name, prof_image, prof_age, lang_id, view_movies, view_series, min_age, router) {
-    const queryString = `CALL create_account('${fname}','${lname}','${email}','${password}',${pay_method},${sub_id},'${prof_name}','${prof_image}',${prof_age},${lang_id},${view_movies},${view_series},${min_age})`;
+  function executeCreateAccount(fname, lname, email, password, pay_method, sub_id, prof_name, prof_image, prof_age, lang_id, view_movies, view_series, min_age, router) {
+      const queryString = `CALL create_account('${fname}','${lname}','${email}','${password}',${pay_method},${sub_id},'${prof_name}','${prof_image}',${prof_age},${lang_id},${view_movies},${view_series},${min_age})`;
 
-    query.outputJSON(queryString, router);
+      query.outputJSON(queryString, router);
+  }
+
+  // Usage:
+  executeCreateAccount('Fname', 'Lname', 'mail@mail.mail', 'password', 0, 0, 'profile name', 'profile image', 18, 0, 0, 0, 0, router);
+
+  hashPassword(password).then(hashed_password => {
+    query.outputJSON(`CALL create_account_small('${first_name}', '${last_name}', '${email}','${hashed_password}';`, router);
+  })
+  .catch(err => {
+    console.error('Error hashing password:', err);
+  });
+});
+
+function splitFullName(fullName) {
+  const nameArray = fullName.split(/\s+/);
+  const firstName = nameArray[0];
+  const lastName = nameArray.slice(1).join(' ');
+  return { firstName, lastName };
 }
 
-// Usage:
-executeCreateAccount('Fname', 'Lname', 'mail@mail.mail', 'password', 0, 0, 'profile name', 'profile image', 18, 0, 0, 0, 0, router);
-
+async function hashPassword(password) {
+  return await bcrypt.hash(password, 10);
+};
 
 module.exports = router;

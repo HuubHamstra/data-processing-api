d4a0dabc9c38d492ef8efe216fc6c071f9902f2b
KevinSpa2 <kevin.spalink@student.nhlstenden.com>
2024-01-16 14:42:59 +0100

    Check received data before comparing passwords

--- hbomin/routes/login.js
+++ hbomin/routes/login.js
@@ -10,26 +10,41 @@ const { secretKey, refreshTokens } = require('./config'); // Import shared confi
 router.post('/', async (req, res) => {
   const { email, password } = req.body;
   const dbQuery = `CALL get_login_data('${email}')`;
-  let login_data = await query.outputJSON(dbQuery, res);
-  received_password = login_data[0][0].password;
 
-  verifyPassword(password, received_password).then(is_match => {
-    if (is_match) {
-      // Generate JWT token upon successful login
-      const accessToken = jwt.sign({ email }, secretKey, { expiresIn: '15m' });
+  try {
+    let login_data = await query.outputJSON(dbQuery, res);
 
-      // Generate and store refresh token
-      const refreshToken = generateRefreshToken();
-      refreshTokens[email] = refreshToken;
-      res.json({ accessToken, refreshToken });
+    if (login_data && login_data[0] && login_data[0][0]) {
+      const received_password = login_data[0][0].password;
+      console.log(received_password);
+      verifyPassword(password, received_password).then((is_match) => {
+        console.log(is_match);
+        if (is_match) {
+          // Generate JWT token upon successful login
+          const accessToken = jwt.sign({ email }, secretKey, {
+            expiresIn: '15m',
+          });
+
+          // Generate and store refresh token
+          const refreshToken = generateRefreshToken();
+          refreshTokens[email] = refreshToken;
+          res.json({ accessToken, refreshToken });
+        } else {
+          res.status(401).json({ message: 'Invalid username or password' });
+        }
+      })
+      .catch(() => {
+        res.status(401).json({ message: 'Invalid username or password' });
+      });
     } else {
       res.status(401).json({ message: 'Invalid username or password' });
     }
-  })
-  .catch(() => {
-    res.status(401).json({ message: 'Invalid username or password' });
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
+
 
 // Function to generate the refresh token
 function generateRefreshToken() {

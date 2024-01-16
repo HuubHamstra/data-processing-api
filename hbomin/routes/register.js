6a291d09df58f933e31081248d3bbdb9c7cae9cb
AmanTrechsel <112391335+AmanTrechsel@users.noreply.github.com>
2024-01-16 13:30:47 +0100

    correct register body reading

--- hbomin/routes/register.js
+++ hbomin/routes/register.js
@@ -4,20 +4,16 @@ const query = require('../query');
 const bcrypt = require('bcrypt');
 
 router.post('/', (req, res) => {
-  const { full_name, email, password } = req.body;
-  const { first_name, last_name } = splitFullName(full_name);
-
-  function executeCreateAccount(fname, lname, email, password, pay_method, sub_id, prof_name, prof_image, prof_age, lang_id, view_movies, view_series, min_age, router) {
-      const queryString = `CALL create_account('${fname}','${lname}','${email}','${password}',${pay_method},${sub_id},'${prof_name}','${prof_image}',${prof_age},${lang_id},${view_movies},${view_series},${min_age})`;
-
-      query.outputJSON(queryString, router);
-  }
-
-  // Usage:
-  executeCreateAccount('Fname', 'Lname', 'mail@mail.mail', 'password', 0, 0, 'profile name', 'profile image', 18, 0, 0, 0, 0, router);
+  const body = req.body;
+  const full_name = body["fullname"];
+  const email = body["email"];
+  const password = body["password"];
+  const split = splitFullName(full_name);
+  const first_name = split["firstName"];
+  const last_name = split["lastName"];
 
   hashPassword(password).then(hashed_password => {
-    query.outputJSON(`CALL create_account_small('${first_name}', '${last_name}', '${email}','${hashed_password}';`, router);
+    query.outputJSON(`CALL create_account('${first_name}', '${last_name}', '${email}','${hashed_password}', 0, 0, 'profile name', 'profile image', 18, 0, 0, 0, 0;`, router);
   })
   .catch(err => {
     console.error('Error hashing password:', err);

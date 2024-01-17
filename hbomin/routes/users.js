f0fac52fc7d23d1cf48481e7d1d7092f723e42cc
KevinSpa2 <kevin.spalink@student.nhlstenden.com>
2024-01-17 20:19:36 +0100

    Get Userlist from database

--- hbomin/routes/users.js
+++ hbomin/routes/users.js
@@ -1,7 +1,17 @@
-var express = require('express');
-var router = express.Router();
-var query = require('../query');
+const express = require('express');
+const router = express.Router();
+const query = require('../query');
 
-query.outputJSON('CALL get_login_data("user@example.com");', router);
+router.get('/', async (req, res) => {
+    const dbQuery = `SELECT account_id AS id, first_name AS firstname, last_name AS lastname, email, payment_method AS subscription FROM account`;
+
+    try {
+        const results = await query.outputJSON(dbQuery);
+        res.json({ results: results });
+    } catch (error) {
+        console.error('Fout bij het uitvoeren van de query: ', error);
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
 
 module.exports = router;

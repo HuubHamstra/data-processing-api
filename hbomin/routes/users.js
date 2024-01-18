2555b43b41cf6b77e77038d47592b8294966c304
AmanTrechsel <112391335+AmanTrechsel@users.noreply.github.com>
2024-01-18 10:59:37 +0100

    email validated added

--- hbomin/routes/users.js
+++ hbomin/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const query = require('../query');
 
 router.get('/', async (req, res) => {
-    const dbQuery = `SELECT account_id AS id, first_name AS firstname, last_name AS lastname, email, payment_method AS subscription FROM account`;
+    const dbQuery = `SELECT account_id AS id, first_name AS firstname, last_name AS lastname, email, email_validated, payment_method AS subscription FROM account`;
 
     try {
         const results = await query.outputJSON(dbQuery);
